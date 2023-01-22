8f0c08a26777be6a05cc7a7f44951a9dbc1a2c04
SomyaGautam02 <somyagautam02@gmail.com>
2023-01-22 15:27:04 +0530

    Added fortuner

--- car/src/App.js
+++ car/src/App.js
@@ -1,9 +1,13 @@
+import "./App.css"
+
 function App(){
     return(
         <div>
             <div className="cars">
                 <h1>Fortuner</h1>
+                <p>The new Fortuner boasts of a power-packed 6-speed Diesel and Petrol AT as well as MT engine capable of turning mountains into molehills. With so much raw, unbridled power at your beck and call, the new Fortuner makes blazing new trails and conquering uncharted territory as easy as a walk in the park</p>
             </div>
+    
         </div>
     );
 }
